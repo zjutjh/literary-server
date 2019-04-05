011b76bcbc90fb60fa99a4f69d31037ecbc68eb0
cccRaim <chen.raim@qq.com>
2019-04-06 02:23:10 +0800

    增加openid自动登录

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 use App\User;
+use App\UserLink;
 use Validator;
 use JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
@@ -33,6 +34,90 @@ class LoginController extends Controller
      */
     public function __construct() {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function loginByWeappOpenid(Request $request) {
+        $messages = [
+            'openid' => '参数错误'
+        ];
+        $validator = Validator::make($request->all(), [
+            'openid' => 'required'
+        ], $messages);
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            return RJM(1, null, $errors->first());
+        }
+        $openid = $request->get('openid');
+        if ($userLink = UserLink::where('openid', $openid)->where('type', 'weapp')->first()) {
+            $user = User::where('id', $userLink->uid)->first();
+            try {
+                if (!$token = JWTAuth::fromUser($user)) {
+                    return RJM(1, null, '用户错误');
+                }
+            } catch (JWTException $e) {
+                // something went wrong whilst attempting to encode the token
+                return RJM(1, null, 'token生成错误');
+            }
+
+            return RJM(0, [
+                'token' => $token
+            ], '自动登录成功');
+        } else {
+            return RJM(1, null, '找不到绑定关系');
+        }
+    }
+
+    public function loginWithOpenid(Request $request) {
+        $messages = [
+            'username.required' => '用户名不能为空',
+            'password.required' => '用户名不能为空',
+            'openid' => '缺少openid',
+            'type' => '缺少类型',
+        ];
+        $validator = Validator::make($request->all(), [
+            'username' => 'required',
+            'password' => 'required',
+            'openid' => 'required',
+            'type' => 'required'
+        ], $messages);
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            return RJM(1, null, $errors->first());
+        }
+        $username = $request->get('username');
+        $password = $request->get('password');
+        $openid = $request->get('openid');
+        $type = $request->get('type');
+        try {
+            JHService::login($username, $password);
+        } catch (\Exception $e) {
+            return RJM(1, null, $e->getMessage());
+        }
+
+        // 检测是否存在用户，不存在则创建
+        if(!$user = User::where('sid', $username)->first()) {
+            $user = new User;
+            $user->sid = $username;
+            $user->save();
+            $userLink = new UserLink;
+            $userLink->uid = $user->id;
+            $userLink->type = $type;
+            $userLink->openid = $openid;
+            $userLink->save();
+        }
+
+        try {
+            if (!$token = JWTAuth::fromUser($user)) {
+                return RJM(1, null, '用户错误');
+            }
+        } catch (JWTException $e) {
+            // something went wrong whilst attempting to encode the token
+            return RJM(1, null, 'token生成错误');
+        }
+
+        return RJM(0, [
+            'token' => $token
+        ]);
     }
 
     public function login(Request $request) {
