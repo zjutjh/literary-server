bf96820bed7807735395827bf8abaf8faf15ecb0
wangwangbu <924228507@qq.com>
2019-09-30 21:43:26 +0800

    修改已报名列表不显示错误

--- app/Http/Controllers/BookPartyController.php
+++ app/Http/Controllers/BookPartyController.php
@@ -53,7 +53,7 @@ class BookPartyController extends Controller
 
     public function getCheckinListByUser(Request $request) {
         $user = $request->user();
-        $list = BookPartyCheckin::where('uid', $user->id)->orderBy('desc')->get();
+        $list = BookPartyCheckin::where('uid', $user->id)->orderBy('id', 'desc')->get();
         foreach ($list as $key => $value) {
             $list[$key] = BookParty::getBookPartyWhenLogin($value->book_party_id, $user->id);
         }
